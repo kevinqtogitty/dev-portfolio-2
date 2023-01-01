6edb2839e4cdf80bae9e32df9368b46360c89f2c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-01 23:48:37 +0100

    Initial commit

---
+++ src/components/react/HamburgerMenu.tsx
@@ -0,0 +1,80 @@
+import { a, useSpring, useTrail } from '@react-spring/web';
+import { Close, Menu } from '@styled-icons/ionicons-outline';
+import type React from 'react';
+import { useState } from 'react';
+
+const data = ['Main', 'Projects', 'Skills', 'About', 'Contact'];
+
+const HamburgerMenu = (ref: React.MutableRefObject<HTMLElement>) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpen((state) => !state);
+  };
+
+  return (
+    <nav className="mobile-menu">
+      <button
+        className="mobile-menu-toggle"
+        onClick={() => handleOpenMenu()}
+        aria-controls="hamburger-primary-navigation"
+        aria-label="Open hamburger menu"
+      >
+        <Menu className="mobile-menu-open" />
+      </button>
+      <SlidingMenu handleOpenMenu={handleOpenMenu} open={open} />
+    </nav>
+  );
+};
+
+interface Props {
+  handleOpenMenu: () => void;
+  open: boolean;
+}
+
+const SlidingMenu: React.FC<Props> = ({ handleOpenMenu, open }) => {
+  const props = useSpring({
+    opacity: open ? 1 : 0,
+    transform: open ? 'translateX(0%)' : 'translateX(100%)'
+  });
+
+  const trail = useTrail(data.length, {
+    transform: open ? 'translateX(0%)' : 'translateX(100%)'
+  });
+
+  return (
+    <a.div
+      style={{ ...props }}
+      className="mobile-sliding-menu"
+      id="hamburger-primary-navigation"
+    >
+      <div className="mobile-h2-close-container">
+        <h2 className="mobile-menu-h2">Menu</h2>
+        <button
+          className="mobile-menu-toggle"
+          onClick={() => handleOpenMenu()}
+          aria-controls="hamburger-primary-navigation"
+          aria-label="Close hamburger menu"
+          aria-expanded={open ? 'true' : 'false'}
+        >
+          <Close className="mobile-menu-close" />
+        </button>
+      </div>
+      <ul className="mobile-menu-ul">
+        {trail.map((spring, i) => (
+          <a.li
+            style={{ ...spring }}
+            key={i}
+            tabIndex={i}
+            className="mobile-menu-li"
+            onClick={() => handleOpenMenu()}
+          >
+            {data[i]}
+          </a.li>
+        ))}
+      </ul>
+    </a.div>
+  );
+};
+
+export default HamburgerMenu;
