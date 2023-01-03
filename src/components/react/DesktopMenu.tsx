df66a867be5dbc232036b8b7c8ef7197e70e9763
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-03 22:39:24 +0100

    Update: added resume fot nav

--- src/components/react/DesktopMenu.tsx
+++ src/components/react/DesktopMenu.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import resume from '../../assets/documents/KevinToResume.pdf';
 
-const data = ['Main', 'Projects', 'Skills', 'About', 'Contact'];
+const data = ['Main', 'About', 'Projects', 'Skills', 'Contact'];
 
 const DesktopMenu = () => {
   return (
@@ -10,11 +11,19 @@ const DesktopMenu = () => {
       <NavigationMenu.List className="desktop-menu-list">
         {data.map((item, i) => (
           <NavigationMenu.Item className="desktop-menu-item" tabIndex={i}>
-            <NavigationMenu.Link className="desktop-menu-link">
-              {item}
+            <NavigationMenu.Link
+              className="desktop-menu-link"
+              href={`#page-${i}`}
+            >
+              <span className="menu-item-number">0{i + 1}.</span> {`${item}`}
             </NavigationMenu.Link>
           </NavigationMenu.Item>
         ))}
+        <div className="hero-button-container resume">
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            <button className="hero-button resume">Resume</button>
+          </a>
+        </div>
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
